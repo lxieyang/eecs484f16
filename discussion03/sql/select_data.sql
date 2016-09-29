d29abbd150df7cb334b85c9c8f9c27a85a184407
lxieyang <lxieyang@umich.edu>
2016-09-29 13:55:42 -0400

    try

--- discussion03/sql/select_data.sql
+++ discussion03/sql/select_data.sql
@@ -5,7 +5,7 @@ WHERE I1.iid = T1.iid AND I2.iid = T2.iid AND
       T1.cid <> T2.cid AND I1.iid <> I2.iid;
 
 
-SELECT I1.iid, I2.iid
+SELECT DISTINCT I1.iid, I2.iid
 FROM Instructor I1, Instructor I2,
      Teaches T1, Teaches T2
 WHERE (I1.iid = T1.iid AND I2.iid = T2.iid AND
