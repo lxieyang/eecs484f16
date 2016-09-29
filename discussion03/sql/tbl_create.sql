3d93ba7fba19d27ede181ad53f856b7adcd7f783
lxieyang <lxieyang@umich.edu>
2016-09-29 13:28:04 -0400

    try fix

---
+++ discussion03/sql/tbl_create.sql
@@ -0,0 +1,30 @@
+DROP TABLE Instructor;
+DROP TABLE Course;
+DROP TABLE Teaches;
+
+
+-- Instructor Table
+CREATE TABLE Instructor (
+  iid INTEGER,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  PRIMARY KEY (iid)
+);
+
+
+-- Course Table
+CREATE TABLE Course (
+  cid INTEGER,
+  course_name VARCHAR(100),
+  PRIMARY KEY (cid)
+);
+
+
+-- Teaches Table
+CREATE TABLE Teaches (
+  iid INTEGER,
+  cid INTEGER,
+  PRIMARY KEY (iid, cid),
+  FOREIGN KEY (iid) REFERENCES Instructor(iid) ON DELETE CASCADE,
+  FOREIGN KEY (cid) REFERENCES Course(cid) ON DELETE CASCADE
+);
