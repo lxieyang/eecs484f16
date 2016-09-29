0b8a5227d2218f7cdced23efe6f7aad856e7c6e4
lxieyang <lxieyang@umich.edu>
2016-09-29 13:41:22 -0400

    try

--- discussion03/sql/select_data.sql
+++ discussion03/sql/select_data.sql
@@ -8,6 +8,8 @@ WHERE I1.iid = T1.iid AND I2.iid = T2.iid AND
 SELECT I1.iid, I2.iid
 FROM Instructor I1, Instructor I2,
      Teaches T1, Teaches T2
-WHERE I1.iid = T1.iid AND I2.iid = T2.iid AND
+WHERE (I1.iid = T1.iid AND I2.iid = T2.iid AND
       T1.cid <> T2.cid AND I1.iid <> I2.iid AND
-      I1.iid < I2.iid;
+      I1.iid < I2.iid)
+      OR I1.iid = T1.iid AND I2.iid NOT IN
+      (SELECT * FROM Teaches);
